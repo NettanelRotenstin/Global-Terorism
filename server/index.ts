3c22bbdefddc340c355cfbeed5b138de372dacd8
Netanel Rotenstein <email@example.com>
2024-12-18 12:27:33 +0200

    index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,19 @@
-console.log(1)
+import express, { Request, Response } from 'express';
+import { connentToMongo } from './DB/config';
+import cors from 'cors'
+
+const PORT = process.env.PORT
+const app = express();
+
+connentToMongo();
+
+app.use(cors());
+app.use(express.json());
+
+ 
+
+ 
+
+app.listen(PORT, () => {
+  console.log(`[server] I'm up on port ${PORT}`);
+});
