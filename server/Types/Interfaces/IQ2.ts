4cce8ceaeea685b329574abb2ff9cbaf0a744a03
Netanel Rotenstein <email@example.com>
2024-12-18 11:19:53 +0200

    interface for q2 nodel

---
+++ server/Types/Interfaces/IQ2.ts
@@ -0,0 +1,9 @@
+import ILocation from "./ILocation"
+
+export default interface IQ2{
+    region:string
+    numCasualties:number
+    country:string
+    city:string
+    locationArr:ILocation[]
+}
