20f5800633c85c01804f9fd08f943bbb12b497be
Netanel Rotenstein <email@example.com>
2024-12-19 07:50:43 +0200

    ceed done

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { connentToMongo } from './DB/config';
 import cors from 'cors'
-import { ceedSchema1, ceedSchema1Attack2, ceedSchema2, ceedSchema3, getFileData } from './Services/ceedDataService';
+import { ceedOrgan, ceedSchema1, ceedSchema1Attack2, ceedSchema2, ceedSchema3, ceedSchema4, ceedSchema5, getFileData } from './Services/ceedDataService';
  
 const PORT = process.env.PORT
 const app = express();
@@ -10,10 +10,8 @@ connentToMongo();
 
 app.use(cors());
 app.use(express.json());
-//ceedSchema1()
-//ceedSchema1Attack2()
-//ceedSchema2()
-//ceedSchema3()
+ 
+ 
 app.listen(PORT, () => {
   console.log(`[server] I'm up on port ${PORT}`);
 });
