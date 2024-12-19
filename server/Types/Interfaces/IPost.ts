9b437851587c1bdb05a1c0bacaae7dd19441cfe7
Netanel Rotenstein <email@example.com>
2024-12-19 12:14:11 +0200

    interface for post

---
+++ server/Types/Interfaces/IPost.ts
@@ -0,0 +1,13 @@
+export default interface IPost{
+    attackType?:string
+    nkill?:number
+    mwound?:number
+    region:string
+    country?:string
+    city?:string
+    lat:number
+    lon:number
+    organName:string
+    year:number
+    month:number
+}
