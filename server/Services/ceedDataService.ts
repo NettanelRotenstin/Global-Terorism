a8b35e6d42dcedb9fd9aeafa21c11520700f1a16
Netanel Rotenstein <email@example.com>
2024-12-20 10:11:33 +0200

    ceed fixing funcs

--- server/Services/ceedDataService.ts
+++ server/Services/ceedDataService.ts
@@ -10,7 +10,22 @@ import { orgaAndLocateModel } from '../Models/orgaAndLocateModel'
 import { q4Model } from '../Models/q4Model'
 import { q5Model } from '../Models/q5Model'
 import { q6Model } from '../Models/q6Model'
+import ISummary from '../Types/Interfaces/ISummary'
+import { summaryModel } from '../Models/summaryModel'
 
+// export const ceedAll = async () => {
+//     Promise.all([
+//         ceedSchema1(),
+//         ceedSchema1Attack2(),
+//         ceedSchema2(),
+//         ceedOrgan(),
+//         ceedSchema3(),
+//         ceedSchema4(),
+//         ceedSchema5(),
+//         ceedSchema6(),
+//         ceedSummary(),
+//         console.log("ceed done!")])
+// }
 export const getFileData = async <T>(): Promise<T[] | void> => {
     try {
         const dataFromFile: any = (await fs.readFile(`/Users/netanelrotenstein/Desktop/fullStuck/Global Terorism/server/Data/globalterrorismdb_0718dist.json`, 'utf-8'));
@@ -21,133 +36,96 @@ export const getFileData = async <T>(): Promise<T[] | void> => {
     }
 }
 
-//ceed the kindattack1 to the db model
-export const ceedSchema1 = async (): Promise<void> => {
+export const ceedSameTime1 = async () => {
     try {
         const data: any = await getFileData()
-
         let casualties: number = 0
         for (const element of data as any[]) {
             casualties = calcCasualties(element.nkill, element.nwound)
-            let existing: mongoose.AnyObject | null = await q1Model.findOne({ attackType: element.attacktype1_txt })
-            if (!existing) {
+            let existingQ1: mongoose.AnyObject | null = await q1Model.findOne({ attackType: element.attacktype1_txt })
+            if (!existingQ1) {
                 const newQ1 = new q1Model({ attackType: element.attacktype1_txt, numCasualties: casualties })
                 await newQ1.save()
             }
             else {
-                existing.numCasualties += casualties
-                await existing.save()
+                existingQ1.numCasualties += casualties
+                await existingQ1.save()
             }
-        }
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+            //end schema 1.0
 
-//ceed the kindattack2 to the db model
-export const ceedSchema1Attack2 = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        let casualties: number = 0
-        for (const element of data as any[]) {
-            casualties = calcCasualties(element.nkill, element.nwound)
-            if (typeof (element.attacktype2_txt) != typeof ("hvh")) { continue }
-            let existing: mongoose.AnyObject | null = await q1Model.findOne({ attackType: element.attacktype2_txt })
-            if (!existing) {
-                const newQ1 = new q1Model({ attackType: element.attacktype2_txt, numCasualties: casualties })
-                await newQ1.save()
-            }
-             
-            else {
-                existing.numCasualties += casualties
-                await existing.save()
-            }
-        }
-        console.log('1')
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-//ceed for schema2
-export const ceedSchema2 = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        let casualties: number = 0
-        for (const element of data as any[]) {
-            casualties = calcCasualties(element.nkill, element.nwound)
-            let existing: mongoose.AnyObject | null = await q2Model.findOne({ region: element.region_txt, country: element.country_txt, city: element.city })
+            let existingQ2 = await q2Model.findOne({ region: element.region_txt, country: element.country_txt, city: element.city })
             const location = new locationModel({ lat: element.latitude, lon: element.longitude })
             await location.save()
-            if (!existing) {
+            if (!existingQ2) {
                 const newQ2 = new q2Model({ region: element.region_txt, country: element.country_txt, city: element.city, locationArr: [location] })
                 await newQ2.save()
             }
             else {
-                existing.numCasualties += casualties
-                existing.locationArr.push(location)
-                await existing.save()
+                existingQ2.numCasualties += casualties
+                existingQ2.locationArr.push(location)
+                await existingQ2.save()
             }
-        }
-        console.log(2)
-    } catch (error) {
-        console.log(error)
-    }
-}
+            //end schema 2
 
-//ceed for schema3
-export const ceedSchema3 = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        for (const element of data as any[]) {
-            let existing: mongoose.AnyObject | null = await q3Model.findOne({ year: element.iyear, month: element.imonth })
-            if (!existing) {
+            let existingQ3: mongoose.AnyObject | null = await q3Model.findOne({ year: element.iyear, month: element.imonth })
+            if (!existingQ3) {
                 const newQ3 = new q3Model({ year: element.iyear, month: element.imonth, numEvent: 1 })
                 await newQ3.save()
             }
             else {
-                existing.numEvent = existing.numEvent + 1
-                await existing.save()
+                existingQ3.numEvent = existingQ3.numEvent + 1
+                await existingQ3.save()
             }
-        }
-        console.log(3)
-    } catch (error) {
-        console.log(error)
-    }
-}
+            //end schema 3
 
-//ceed for schema4
-export const ceedOrgan = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        for (const element of data as any[]) {
-            let existing: mongoose.AnyObject | null = await orgaAndLocateModel.findOne({ region: element.region_txt, organName: element.gname })
-            if (!existing) {
+            let existingOrg: mongoose.AnyObject | null = await orgaAndLocateModel.findOne({ region: element.region_txt, organName: element.gname })
+            if (!existingOrg) {
                 const newOrga = new orgaAndLocateModel({ region: element.region_txt, organName: element.gname, numEvent: 1 })
                 await newOrga.save()
             }
             else {
-                existing.numEvent += 1
-                await existing.save()
+                existingOrg.numEvent += 1
+                await existingOrg.save()
+            }
+            //end schema orga
+
+            let existingQ5: mongoose.AnyObject | null = await q5Model.findOne({ year: element.iyear, organizationName: element.gname })
+            if (!existingQ5) {
+                const newQ5 = new q5Model({ year: element.iyear, organizationName: element.gname, numEvent: 1 })
+                await newQ5.save()
+            }
+            else {
+                existingQ5.numEvent = existingQ5.numEvent + 1
+                await existingQ5.save()
+            }
+            //end schema 5
+
+            let existingQ6: mongoose.AnyObject | null = await q6Model.findOne({ region: element.region_txt, organName: element.gname })
+            if (!existingQ6) {
+                const newQ6 = new q6Model({ region: element.region_txt, organName: element.gname, numCasualties: casualties })
+                await newQ6.save()
+            }
+            else {
+                existingQ6.numCasualties = existingQ6.numCasualties + casualties
+                await existingQ6.save()
             }
         }
-        
+        console.log('ceed 1 done!')
     } catch (error) {
-        console.log(error)
+
     }
+
 }
 
-
-
-
-//ceed for schema4
-export const ceedSchema4 = async (): Promise<void> => {
+export const ceedSameTime2 = async()=>{
     try {
         const data: any = await getFileData()
+        let casualties2: number = 0
         for (const element of data as any[]) {
-            let existing: mongoose.AnyObject | null = await q4Model.findOne({ region: element.region_txt })
-            if (!existing) {
+             
+
+            let existingQ4: mongoose.AnyObject | null = await q4Model.findOne({ region: element.region_txt })
+            if (!existingQ4) {
                 const newQ4 = new q4Model({ region: element.region_txt })
                 let orgs = await orgaAndLocateModel.find({ region: element.region_txt }).sort({ numEvent: -1 })
                 newQ4.organizeTopFive.push(orgs[0]._id as any, orgs[1]._id as any, orgs[2]._id as any, orgs[3]._id as any, orgs[4]._id as any)
@@ -156,64 +134,186 @@ export const ceedSchema4 = async (): Promise<void> => {
             else {
                 continue
             }
+            //end schema 4
         }
-        console.log(4)
+        console.log('ceed 2 done!')
     } catch (error) {
         console.log(error)
     }
 }
+// //ceed the kindattack1 to the db model
+// export const ceedSchema1 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
 
-//ceed for schema5
-export const ceedSchema5 = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        for (const element of data as any[]) {
-            let existing: mongoose.AnyObject | null = await q5Model.findOne({ year: element.iyear, organizationName: element.gname })
-            if (!existing) {
-                const newQ5 = new q5Model({ year: element.iyear, organizationName: element.gname, numEvent: 1 })
-                await newQ5.save()
-            }
-            else {
-                existing.numEvent = existing.numEvent + 1
-                await existing.save()
-            }
-        }
-        console.log(5)
-    } catch (error) {
-        console.log(error)
-    }
-}
+//         let casualties: number = 0
+//         for (const element of data as any[]) {
+//             casualties = calcCasualties(element.nkill, element.nwound)
+//             let existing: mongoose.AnyObject | null = await q1Model.findOne({ attackType: element.attacktype1_txt })
+//             if (!existing) {
+//                 const newQ1 = new q1Model({ attackType: element.attacktype1_txt, numCasualties: casualties })
+//                 await newQ1.save()
+//             }
+//             else {
+//                 existing.numCasualties += casualties
+//                 await existing.save()
+//             }
+//         }
 
-//ceed for schema6
-export const ceedSchema6 = async (): Promise<void> => {
-    try {
-        const data: any = await getFileData()
-        let casualties: number = 0
-        for (const element of data as any[]) {
-            casualties = await calcCasualties(element.nkill, element.nwound)
-            let existing: mongoose.AnyObject | null = await q6Model.findOne({ region: element.region_txt, organName: element.gname })
-            if (!existing) {
-                const newQ6 = new q6Model({ region: element.region_txt, organName: element.gname ,numCasualties:casualties })
-                await newQ6.save()
-            }
-            else {
-                existing.numCasualties = existing.numCasualties + casualties
-                await existing.save()
-            }
-        }
-        console.log(6)
-    } catch (error) {
-        console.log(error)
-    }
-}
- 
-// //ceed for schema6
-// export const ceedSommary = async (): Promise<void> => {
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed the kindattack2 to the db model
+// export const ceedSchema1Attack2 = async (): Promise<void> => {
 //     try {
 //         const data: any = await getFileData()
 //         let casualties: number = 0
 //         for (const element of data as any[]) {
-//                 const newQ6 = new q6Model({ region: element.region_txt, organName: element.gname ,numCasualties:casualties })
+//             casualties = calcCasualties(element.nkill, element.nwound)
+//             if (typeof (element.attacktype2_txt) != typeof ("hvh")) { continue }
+//             let existing: mongoose.AnyObject | null = await q1Model.findOne({ attackType: element.attacktype2_txt })
+//             if (!existing) {
+//                 const newQ1 = new q1Model({ attackType: element.attacktype2_txt, numCasualties: casualties })
+//                 await newQ1.save()
+//             }
+
+//             else {
+//                 existing.numCasualties += casualties
+//                 await existing.save()
+//             }
+//         }
+//         console.log('1')
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed for schema2
+// export const ceedSchema2 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         let casualties: number = 0
+//         for (const element of data as any[]) {
+//             casualties = calcCasualties(element.nkill, element.nwound)
+//             let existing: mongoose.AnyObject | null = await q2Model.findOne({ region: element.region_txt, country: element.country_txt, city: element.city })
+//             const location = new locationModel({ lat: element.latitude, lon: element.longitude })
+//             await location.save()
+//             if (!existing) {
+//                 const newQ2 = new q2Model({ region: element.region_txt, country: element.country_txt, city: element.city, locationArr: [location] })
+//                 await newQ2.save()
+//             }
+//             else {
+//                 existing.numCasualties += casualties
+//                 existing.locationArr.push(location)
+//                 await existing.save()
+//             }
+//         }
+//         console.log(2)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed for schema3
+// export const ceedSchema3 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         for (const element of data as any[]) {
+//             let existing: mongoose.AnyObject | null = await q3Model.findOne({ year: element.iyear, month: element.imonth })
+//             if (!existing) {
+//                 const newQ3 = new q3Model({ year: element.iyear, month: element.imonth, numEvent: 1 })
+//                 await newQ3.save()
+//             }
+//             else {
+//                 existing.numEvent = existing.numEvent + 1
+//                 await existing.save()
+//             }
+//         }
+//         console.log(3)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed for schema4
+// export const ceedOrgan = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         for (const element of data as any[]) {
+//             let existing: mongoose.AnyObject | null = await orgaAndLocateModel.findOne({ region: element.region_txt, organName: element.gname })
+//             if (!existing) {
+//                 const newOrga = new orgaAndLocateModel({ region: element.region_txt, organName: element.gname, numEvent: 1 })
+//                 await newOrga.save()
+//             }
+//             else {
+//                 existing.numEvent += 1
+//                 await existing.save()
+//             }
+//         }
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+
+
+
+// //ceed for schema4
+// export const ceedSchema4 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         for (const element of data as any[]) {
+//             let existing: mongoose.AnyObject | null = await q4Model.findOne({ region: element.region_txt })
+//             if (!existing) {
+//                 const newQ4 = new q4Model({ region: element.region_txt })
+//                 let orgs = await orgaAndLocateModel.find({ region: element.region_txt }).sort({ numEvent: -1 })
+//                 newQ4.organizeTopFive.push(orgs[0]._id as any, orgs[1]._id as any, orgs[2]._id as any, orgs[3]._id as any, orgs[4]._id as any)
+//                 await newQ4.save()
+//             }
+//             else {
+//                 continue
+//             }
+//         }
+//         console.log(4)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed for schema5
+// export const ceedSchema5 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         for (const element of data as any[]) {
+//             let existing: mongoose.AnyObject | null = await q5Model.findOne({ year: element.iyear, organizationName: element.gname })
+//             if (!existing) {
+//                 const newQ5 = new q5Model({ year: element.iyear, organizationName: element.gname, numEvent: 1 })
+//                 await newQ5.save()
+//             }
+//             else {
+//                 existing.numEvent = existing.numEvent + 1
+//                 await existing.save()
+//             }
+//         }
+//         console.log(5)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// //ceed for schema6
+// export const ceedSchema6 = async (): Promise<void> => {
+//     try {
+//         const data: any = await getFileData()
+//         let casualties: number = 0
+//         for (const element of data as any[]) {
+//             casualties = await calcCasualties(element.nkill, element.nwound)
+//             let existing: mongoose.AnyObject | null = await q6Model.findOne({ region: element.region_txt, organName: element.gname })
+//             if (!existing) {
+//                 const newQ6 = new q6Model({ region: element.region_txt, organName: element.gname, numCasualties: casualties })
 //                 await newQ6.save()
 //             }
 //             else {
@@ -226,3 +326,17 @@ export const ceedSchema6 = async (): Promise<void> => {
 //         console.log(error)
 //     }
 // }
+
+export const ceedSummary = async () => {
+    try {
+        const data: any = await getFileData();
+        for (const element of data as any[]) {
+            const { eventid, iyear, imonth, iday, country_txt, region_txt, city, latitude, longitude, attacktype1_txt, targtype1_txt, target1, gname, weaptype1_txt, nkill, nwound, nperps, summary } = element;
+            const newSummary = new summaryModel({ eventid, iyear, imonth, iday, country_txt, region_txt, city, latitude, longitude, attacktype1_txt, targtype1_txt, target1, gname, weaptype1_txt, nkill, nwound, nperps, summary })
+            await newSummary.save();
+        }
+        console.log('summery done!');
+    } catch (err) {
+        console.log(err);
+    }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import { connentToMongo } from './DB/config';
 import cors from 'cors'
-import { ceedOrgan, ceedSchema1, ceedSchema1Attack2, ceedSchema2, ceedSchema3, ceedSchema4, ceedSchema5, ceedSchema6, getFileData } from './Services/ceedDataService';
 import 'dotenv/config'
 import { orgaAndLocateModel } from './Models/orgaAndLocateModel';
 import { getQ1Service, getQ2Service, getQ3Service, getQ4ServiceAll, getQ4ServiceArea, getQ6ServiceArea,  } from './Services/getDataFromDB';
@@ -13,6 +12,7 @@ import { Server } from 'socket.io';
 import { handelSocketConnection } from './Socket/funcsForSocket';
 import analystRouter from './Routes/analystRouter';
 import relationshipRouter from './Routes/relationshipRouter';
+import { ceedSameTime1, ceedSameTime2, ceedSummary } from './Services/ceedDataService';
 
 const app = express();
 export const server = http.createServer(app)
@@ -29,8 +29,8 @@ io.on('connection',handelSocketConnection)
 
 app.use("/api/analysis", analystRouter)
 app.use("/api/relationships", relationshipRouter)
- 
-server.listen(process.env.PORT, () => {
+ceedSummary()
+ server.listen(process.env.PORT, () => {
   console.log(`[server] I'm up on port ${process.env.PORT}`);
 });
  
